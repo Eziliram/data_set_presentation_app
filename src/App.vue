3f45a0a51190cdc5389e1d4318f0d3ab0c4cf251
Marilize Franken <mfranken@compuscan.co.za>
2020-01-17 08:54:44 +0200

    Added data file; implemented initial functionality; removed unused components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import $ from 'jquery'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+
+        mounted() {
+            this.processData();
+        },
+
+        data: () => {
+            return {
+
+            }
+        },
+
+        methods: {
+            /**
+             *
+             */
+            async processData() {
+
+                let attributes = [];
+                let users = [];
+
+                // Get the data from the data file
+                await $.get('anonymous-msweb.data', data => {
+
+                    let dataLines = data.split('\n');
+
+                    dataLines.forEach(dataLine => {
+                        if (dataLine.charAt(0) === 'A') attributes.push(this.getAttributes(dataLine.split(',')));
+                        // TODO: store users
+                    });
+                });
+
+                console.log(attributes)
+            },
+            /**
+             *
+             * @param data
+             * @returns {{id: *, title: *, url: *}}
+             */
+            getAttributes(data) {
+
+                let attributes = [];
+
+                data.forEach(attribute => {
+                    // Ignore the elements containing 'A' and '1'
+                    if (attribute === 'A' || attribute === '1') return; //todo: filter this in a better way
+                    attributes.push(attribute.replace(/"/g, ''));
+                });
+
+                return {
+                    id: attributes[0],
+                    title: attributes[1],
+                    url: attributes[2]
+                };
+            },
+            getUsers(data) {
+
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
